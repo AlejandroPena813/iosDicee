ab71ebb705020984e3dbe8dbeaa1d6e085c54c93
Alejandro Pena <alejandropena17@gmail.com>
2019-09-08 15:20:42 -0400

    Push of Full Proj

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    //outlet type, weak=connection, type of var(or let), name, obj type, idk '!'
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateDice()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func RollButton(_ sender: UIButton) {
+        updateDice()
+    }
+    
+    func updateDice(){ // this is bc RollButton expects keypress
+        randomDiceIndex1 = Int.random(in: 0 ... 5) // '...' is range
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1]) //simply name of asset
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    //shake gesture
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        updateDice()
+    }
 }
 
